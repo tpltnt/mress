10f5feef664d23a17d017c6d680072cf794d2a0c
tpltnt <tpltnt.github@dropcut.net>
2014-02-14 22:37:20 +0100

    first compile

--- mress.go
+++ mress.go
@@ -1,10 +1,20 @@
 package main
 
-import(
+import (
+	"flag"
 	"fmt"
 	"github.com/thoj/go-ircevent" // imported as "irc"
 )
 
 func main() {
-	fmt.Println("hello IRC")
+	fmt.Println("starting up ...")
+	configfile := flag.String("config", "", "configuration file")
+	flag.Parse()
+	if nil == configfile {
+		fmt.Println("no config file given")
+	}
+	ircobj := irc.IRC("<nick>", "<user>") //Create new ircobj
+	if nil == ircobj {
+		fmt.Println("no IRC object")
+	}
 }
