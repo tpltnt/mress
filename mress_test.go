635d54416114aa4cf3076f690ee1fbe9127798ca
tpltnt <tpltnt.github@dropcut.net>
2014-02-15 17:31:47 +0100

    logger + tests fixed

--- mress_test.go
+++ mress_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 // test stdout destination
+// TODO acually check stdout
 func Test_create_Logger_1(t *testing.T) {
 	dest := "stdout"
 	logger := createLogger(&dest)
@@ -14,6 +16,7 @@ func Test_create_Logger_1(t *testing.T) {
 }
 
 // test stderr destination
+// TODO actually check stderr
 func Test_create_Logger_2(t *testing.T) {
 	dest := "stderr"
 	logger := createLogger(&dest)
@@ -24,9 +27,28 @@ func Test_create_Logger_2(t *testing.T) {
 
 // test logfile destination
 func Test_create_Logger_3(t *testing.T) {
-	dest := "test.log"
+	dest := "test-logger.log"
 	logger := createLogger(&dest)
 	if logger == nil {
 		t.Error("creating logger to '" + dest + "' returned 'nil'")
+	}
+	logger.Println("basic logger test")
+
+	logfile, err := os.Open(dest)
+	if nil != err {
+		t.Error(err.Error())
+	}
+	filecontent := make([]byte, 100)
+	count, err := logfile.Read(filecontent)
+	if nil != err {
+		t.Error("reading logfile failed: " + err.Error())
+	}
+	if 46 != count {
+		t.Error("read wrong number of bytes")
+	}
+	logfile.Close()
+	err = os.Remove(dest)
+	if nil != err {
+		t.Error(err.Error())
 	}
 }
