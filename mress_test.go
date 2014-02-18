d8e81dc7041642bb995674a7e088a36f03e5ec0c
tpltnt <tpltnt.github@dropcut.net>
2014-02-18 13:50:44 +0100

    another test

--- mress_test.go
+++ mress_test.go
@@ -132,5 +132,27 @@ func Test_deliverOfflineMessage_0(t *testing.T) {
 	os.Remove("./messages.db")
 }
 
+func Test_deliverOfflineMessage_1(t *testing.T) {
+	// prepare db
+	db, err := sql.Open("sqlite3", "./messages.db")
+	if err != nil {
+		t.Error("failed to open database file: " + err.Error())
+	}
+	defer db.Close()
+	sql := `CREATE TABLE IF NOT EXISTS messages (target TEXT, source TEXT, content TEXT);`
+	_, err = db.Exec(sql)
+	if err != nil {
+		t.Error("failed to create database table: " + err.Error())
+	}
+
+	var con *irc.Connection
+	err = deliverOfflineMessage("test user", con)
+	if err == nil {
+		t.Log("username with spaces shouldn't be accepted")
+	}
+
+	os.Remove("./messages.db")
+}
+
 // deliverOfflineMessage(user string, con *irc.Connection) error
 // offlineMessengerCommand(e *irc.Event, irc *irc.Connection, user string, logger *log.Logger)
